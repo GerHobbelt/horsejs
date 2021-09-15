cbefdec17ff1c7f6d67874303b3cbab6decce212
unknown <liuxiaolun@hikvision.com>
2021-09-15 15:09:14 +0800

    第一个示例性Demo完成

--- HorseJs/app/demo/demoPageLogic.js
+++ HorseJs/app/demo/demoPageLogic.js
@@ -1,7 +1,16 @@
 export let demoPageLogic = {
   init: () => {
-    document.querySelector("#menuContainer").addEventListener("click", (e) => {
-      console.log(e);
+    let menuContainer = document.querySelector("#menuContainer");
+    menuContainer.addEventListener("click", (e) => {
+      if (e.target == menuContainer) return;
+      if (e.target.classList.contains("menuSelected")) return;
+      document.querySelector(".menuSelected").classList.remove("menuSelected");
+      e.target.classList.add("menuSelected");
+      let id = e.target.getAttribute("id").replace("menu", "section");
+      document
+        .querySelector(".sectionSelected")
+        .classList.remove("sectionSelected");
+      document.querySelector(`#${id}`).classList.add("sectionSelected");
     });
   },
 };
