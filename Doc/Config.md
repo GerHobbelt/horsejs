7a48784f9e89f59b2936d7d93964d68cbba90db8
unknown <liuxiaolun@hikvision.com>
2021-10-11 11:02:18 +0800

    更新Config的文档 & windowDelegate.cpp

--- Doc/Config.md
+++ Doc/Config.md
@@ -1,5 +1,3 @@
-# 配置文件
-
 配置文件可有可无，如下是全量的配置文件示例，
 
 示例中所有配置项的值即为默认值，也就是说，如果你没有设置此项配置，程序将按该默认值所代表的意义执行
@@ -11,11 +9,52 @@
   "appName": "HorseJs",
   "startPath": "app/index.html",
   "autoShowFirstWindow": false,
-  "version": "0.0.1"
+  "version": "0.0.1",
+  "sizeConfig": {
+    "miniSize": {
+      "width": 400,
+      "height": 300
+    },
+    "maxSize": {
+      "width": -1,
+      "height": -1
+    },
+    "startSize": {
+      "width": 800,
+      "height": 600
+    }
+  }
 }
 ```
 
-- appName：应用程序名称
-- startPath：第一个窗口的启动路径，必须是相对于可执行文件的子路径
-- autoShowFirstWindow：是否启动即马上显示第一个窗口，设置成 false 后续开发者自己调用 horse.window.show();来显示窗口
-- version：当前应用程序的版本号
+# appName
+
+应用程序名称
+
+# startPath
+
+第一个窗口的启动路径，必须是相对于可执行文件的子路径
+
+# autoShowFirstWindow
+
+是否启动即马上显示第一个窗口，设置成 false 后续开发者自己调用 horse.window.show();来显示窗口
+
+# version
+
+当前应用程序的版本号
+
+# sizeConfig
+
+第一个窗口的尺寸配置
+
+## miniSize
+
+第一个窗口的最小尺寸，width 宽度，height 高度
+
+## maxSize
+
+第一个窗口的最大尺寸，width 宽度，height 高度
+
+## startSize
+
+第一个窗口的默认尺寸，width 宽度，height 高度
---
+++ HorseJs/Browser/WindowDelegate.cpp
@@ -0,0 +1,52 @@
+#include "WindowDelegate.h"
+void WindowDelegate::OnWindowCreated(CefRefPtr<CefWindow> window)
+{
+    window->AddChildView(browser_view_);
+    auto config = Config::get();
+    auto autoShowFirstWindow = config["autoShowFirstWindow"].get<bool>();
+    if (autoShowFirstWindow) {
+        window->Show();
+    }
+    //CefSize size{ 800,600 };
+    //window->CenterWindow(size);
+    window->CenterWindow(window->GetSize());
+    CefRefPtr<CefImage> image = CefImage::CreateImage();
+    PrepareIcon(image, 1.0f, "icon1.png");
+    PrepareIcon(image, 2.0f, "icon2.png");
+    window->SetWindowAppIcon(image);
+    browser_view_->RequestFocus();
+}
+bool WindowDelegate::CanClose(CefRefPtr<CefWindow> window)
+{
+    CefRefPtr<CefBrowser> browser = browser_view_->GetBrowser();
+    if (browser)
+        return browser->GetHost()->TryCloseBrowser();
+    return true;
+}
+
+CefSize WindowDelegate::GetPreferredSize(CefRefPtr<CefView> view){
+    return getSizeConfig("startSize");
+}
+CefSize WindowDelegate::GetMinimumSize(CefRefPtr<CefView> view) {
+    return getSizeConfig("miniSize");
+}
+CefSize WindowDelegate::GetMaximumSize(CefRefPtr<CefView> view) {
+    return getSizeConfig("maxSize");
+}
+CefSize WindowDelegate::getSizeConfig(std::string_view configName) {
+    json miniSize = Config::get()["sizeConfig"][std::string(configName)];
+    int width = miniSize["width"].get<int>();
+    int height = miniSize["height"].get<int>();
+    return CefSize(width, height);
+}
+void WindowDelegate::PrepareIcon(CefRefPtr<CefImage> image, double scale_factor, const std::string& name) {
+    auto curWorkDir = std::filesystem::current_path();
+    curWorkDir.append(name);
+    std::ifstream reader;
+    reader.open(curWorkDir, std::ios::binary);
+    std::stringstream buffer;
+    buffer << reader.rdbuf();
+    reader.close();
+    std::string data = buffer.str();
+    image->AddPNG(scale_factor, data.c_str(), data.size());
+}
