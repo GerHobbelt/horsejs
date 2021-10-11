649fb8deb0daf31c8231ef13b7cf13e52be6b533
unknown <liuxiaolun@hikvision.com>
2021-10-11 12:40:59 +0800

    系统默认配置与用户自定义配置整合逻辑

--- HorseJs/Common/Config.h
+++ HorseJs/Common/Config.h
@@ -3,6 +3,10 @@
 #include <fstream>
 #include <filesystem>
 #include <iostream>
+#include <wx/stdpaths.h>
+#include <wx/filename.h>
+#include <wx/file.h>
+
 using nlohmann::json;
 class Config
 {
@@ -13,17 +17,19 @@ public:
     static json get() {
         static json  config_;
         if (!config_.is_null()) return config_;
-        auto targetPath = std::filesystem::current_path();
-        //todo 如果没有这个文件
-        targetPath.append("app/horse.config.json");
-        std::ifstream reader;
-        reader.open(targetPath, std::ios::in);
-        //todo open失败
-        std::stringstream buffer;
-        buffer << reader.rdbuf();
-        reader.close();
-        auto configCode = buffer.str();
-        config_ = json::parse(configCode);
+        auto userConfigPath = std::filesystem::current_path().append("app/horse.config.json").wstring();
+        auto defaultConfigPath = std::filesystem::current_path().append("DefaultConfig.json").wstring();
+        wxFile defaultConfigFile(defaultConfigPath);
+        wxString defaultConfigText;
+        defaultConfigFile.ReadAll(&defaultConfigText);
+        config_ = json::parse(defaultConfigText.ToStdString());
+        if (wxFile::Exists(userConfigPath)) {
+            wxFile userConfigFile(userConfigPath);
+            wxString userConfigText;
+            userConfigFile.ReadAll(&userConfigText);
+            json userConfig = json::parse(userConfigText.ToStdString());
+            config_.merge_patch(userConfig);
+        }
         return config_;
     };
     
