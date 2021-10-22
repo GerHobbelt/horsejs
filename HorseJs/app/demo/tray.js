441bfe027f05f9329945fe04b8e10eb7cd9e37eb
unknown <liuxiaolun@hikvision.com>
2021-10-22 17:06:53 +0800

    开始处理托盘图标的逻辑

--- HorseJs/app/demo/tray.js
+++ HorseJs/app/demo/tray.js
@@ -1,1 +1,34 @@
-export let processor = {};
+export let processor = {
+  create: async () => {
+    alert(12);
+    let menu = [{ name: "test1" }, { name: "测试测试" }, { name: "测试测试" }];
+    let result = await horse.tray.create({
+      iconPath: "",
+      menu,
+      menuClick: (index) => {
+        demoLog(`你点击了第${index}个菜单`);
+      },
+    });
+    demoLog(result);
+  },
+  reset: async () => {
+    let data = [{ name: "test1" }, { name: "测试测试" }, { name: "测试测试" }];
+    let result = await horse.menu.popup({
+      data,
+      click: (index) => {
+        demoLog(`你点击了第${index}个菜单`);
+      },
+    });
+    demoLog(result);
+  },
+  destroy: async () => {
+    let data = [{ name: "test1" }, { name: "测试测试" }, { name: "测试测试" }];
+    let result = await horse.menu.popup({
+      data,
+      click: (index) => {
+        demoLog(`你点击了第${index}个菜单`);
+      },
+    });
+    demoLog(result);
+  },
+};
