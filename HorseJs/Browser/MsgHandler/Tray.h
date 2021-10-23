5883a87e87d571b0a0c5d5147f4610bc77bf14a4
liulun <xland@live.cn>
2021-10-23 19:04:01 +0800

    托盘图标的右键菜单

--- HorseJs/Browser/MsgHandler/Tray.h
+++ HorseJs/Browser/MsgHandler/Tray.h
@@ -37,10 +37,33 @@ public:
             auto iconPath = config["iconPath"].get<std::string>();
             auto tip = config["tip"].get<std::string>();
             taskBarIcon = new wxTaskBarIcon();
+            ClientData* userData = new ClientData();
+            userData->frame = frame;
+            userData->msgName = message->GetName().ToString();
+            taskBarIcon->SetClientObject(userData);
             wxIconLocation location;
             location.SetFileName(wxString::FromUTF8(iconPath));
             wxIcon icon(location);
             taskBarIcon->SetIcon(icon, wxString::FromUTF8(tip));
+            taskBarIcon->Bind(wxEVT_TASKBAR_MOVE, &onTrayEvent);
+            taskBarIcon->Bind(wxEVT_TASKBAR_LEFT_DOWN, &onTrayEvent);
+            taskBarIcon->Bind(wxEVT_TASKBAR_LEFT_DCLICK, &onTrayEvent);
+            taskBarIcon->Bind(wxEVT_TASKBAR_LEFT_UP, &onTrayEvent);
+            taskBarIcon->Bind(wxEVT_TASKBAR_RIGHT_DOWN, &onTrayEvent);
+            taskBarIcon->Bind(wxEVT_TASKBAR_RIGHT_DCLICK, &onTrayEvent);
+            taskBarIcon->Bind(wxEVT_TASKBAR_RIGHT_UP, &onTrayEvent);
+            if (!config["menu"].is_null()){
+                Tray::menu = new wxMenu();
+                Tray::menu->SetClientObject(userData);
+                int menuId = 0;
+                for (auto& menuItem : config["menu"])
+                {
+                    auto name = menuItem["name"].get<std::string>();
+                    Tray::menu->Append(menuId, wxString::FromUTF8(name));
+                    wxTheApp->Bind(wxEVT_MENU, &onMenuClicked, menuId);
+                    menuId += 1;
+                }
+            }
         }
         else if (message_name._Starts_with("reset")) {
 
@@ -58,5 +81,54 @@ public:
         return true;
     }
 private:
-    
+    static wxMenu* menu;
+    static void onMenuClicked(wxCommandEvent& e) {
+        auto id = e.GetId();
+        auto target = wxDynamicCast(e.GetEventObject(), wxMenu);
+        auto obj = static_cast<ClientData*>(target->GetClientObject());
+        CefRefPtr<CefProcessMessage> msg = CefProcessMessage::Create(obj->msgName+"_menu");
+        CefRefPtr<CefListValue> msgArgs = msg->GetArgumentList();
+        json result;
+        result["index"] = id;
+        msgArgs->SetSize(1);
+        msgArgs->SetString(0, result.dump());
+        obj->frame->SendProcessMessage(PID_RENDERER, msg);
+    }
+    static void onTrayEvent(wxTaskBarIconEvent& e) {
+        auto target = wxDynamicCast(e.GetEventObject(), wxTaskBarIcon);
+        auto obj = static_cast<ClientData*>(target->GetClientObject());
+        std::string clickType;
+        if (e.GetEventType() == wxEVT_TASKBAR_MOVE) {
+            clickType = "mouseMove";
+        }
+        else if(e.GetEventType() == wxEVT_TASKBAR_LEFT_DOWN) {
+            clickType = "leftButtonDown";
+        }
+        else if (e.GetEventType() == wxEVT_TASKBAR_LEFT_DCLICK) {
+            clickType = "leftButtonDoubleClick";
+        }
+        else if (e.GetEventType() == wxEVT_TASKBAR_LEFT_UP) {
+            clickType = "leftButtonUp";
+        }
+        else if (e.GetEventType() == wxEVT_TASKBAR_RIGHT_DOWN) {
+            clickType = "rightButtonDown";
+        }
+        else if (e.GetEventType() == wxEVT_TASKBAR_RIGHT_DCLICK) {
+            clickType = "rightButtonDoubleClick";
+        }
+        else if (e.GetEventType() == wxEVT_TASKBAR_RIGHT_UP) {
+            if (Tray::menu) {
+                target->PopupMenu(menu);
+            }            
+            clickType = "rightButtonUp";
+        }
+        CefRefPtr<CefProcessMessage> msg = CefProcessMessage::Create(obj->msgName+"_tray");
+        CefRefPtr<CefListValue> msgArgs = msg->GetArgumentList();
+        json result;
+        result["clickType"] = clickType;
+        msgArgs->SetSize(1);
+        msgArgs->SetString(0, result.dump());
+        obj->frame->SendProcessMessage(PID_RENDERER, msg);
+    }
 };
+wxMenu*  Tray::menu = nullptr;
--- HorseJs/extension/src/Handler/Tray.ts
+++ HorseJs/extension/src/Handler/Tray.ts
@@ -3,13 +3,16 @@ import { Base } from './Base'
 
 export class Tray extends Base {
   className = 'Tray'
-  async create(config: { iconPath: string; tip?: string; menu: [{ name: string }]; menuClick: (index) => {} }) {
+  async create(config: { iconPath: string; tip?: string; menu: [{ name: string }]; menuClick: (index) => void; trayClick?: (index) => void }) {
     return new Promise((resolve, reject) => {
       let msgName = this.createMsgName(this.create)
       eventer.addOnceEventListener(msgName, (result) => {
         resolve({ success: true })
       })
-      eventer.addOnceEventListener(msgName + '_event', (result) => {
+      eventer.addEventListener(msgName + '_tray', (result) => {
+        config.trayClick(result.clickType)
+      })
+      eventer.addEventListener(msgName + '_menu', (result) => {
         config.menuClick(result.index)
       })
       this.callHorseNative(msgName, JSON.stringify(config))
