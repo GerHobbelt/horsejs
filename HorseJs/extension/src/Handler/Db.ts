505bedf72dc3a9a399078c8152d098a844fffc70
unknown <liuxiaolun@hikvision.com>
2021-11-10 16:39:32 +0800

    删除、修改数据

--- HorseJs/extension/src/Handler/Db.ts
+++ HorseJs/extension/src/Handler/Db.ts
@@ -9,20 +9,7 @@ export class Db extends Base {
   close() {
     return this.callHorse(this.close, {})
   }
-  execute(config: { sql: string; onData?: (data) => void }) {
-    return new Promise((resolve, reject) => {
-      let msgName = this.createMsgName(this.execute)
-      eventer.addOnceEventListener(msgName, (result) => {
-        resolve(result)
-      })
-      let dataMsgName = msgName + '_data'
-      eventer.addOnceEventListener(dataMsgName, (result) => {
-        config.onData(result)
-        if (!result.success) {
-          throw new Error(result.info)
-        }
-      })
-      this.callHorseNative(msgName, JSON.stringify(config))
-    })
+  execute(config: { sql: string }) {
+    return this.callHorse(this.execute, config)
   }
 }
