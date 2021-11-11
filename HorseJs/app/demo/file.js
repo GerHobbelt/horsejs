6a63f3ab10dae77f52abd6e7b6aec7f6423d5d41
simon-lundsrb5f <daijiao5892491@163.com>
2021-11-11 13:50:10 +0800

    获取一个目录下的所有路径

---
+++ HorseJs/app/demo/file.js
@@ -0,0 +1,22 @@
+export let processor = {
+  readDir: async () => {
+    let folderResult = await horse.dialog.openFolder({
+      title: "请你先选择一个目录~~~",
+      defaultFilePath: "C:\\",
+      multiSelections: false,
+      filters: ["*"],
+      lastFilterIndex: 0,
+    });
+    if (folderResult.success) {
+      alert(JSON.stringify(folderResult.data));
+    } else {
+      alert("error");
+    }
+    let result = await horse.file.readDir({ folderPath: folderResult.data[0] });
+    if (result.success) {
+      alert(JSON.stringify(result.data));
+    } else {
+      alert("error");
+    }
+  },
+};
