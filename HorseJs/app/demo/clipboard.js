e5f19592927f027e1fcae3270fe5147feffdb364
simon-lundsrb5f <daijiao5892491@163.com>
2021-11-27 11:49:40 +0800

    对话框DEMO完善&DemoLog使用&Info示例完善

--- HorseJs/app/demo/clipboard.js
+++ HorseJs/app/demo/clipboard.js
@@ -1,10 +1,6 @@
 export let processor = {
   getClipboardText: async () => {
     let result = await horse.clipboard.getData({ dataType: "text" });
-    if (result.success) {
-      alert(JSON.stringify(result.data));
-    } else {
-      alert("error");
-    }
+    demoLog(result);
   },
 };
