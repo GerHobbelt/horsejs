2fae6599ecfc6d74e934a2a2d76cede92302f895
simon-lundsrb5f <daijiao5892491@163.com>
2022-07-22 01:24:22 +0800

    demo重构

--- HorseJs/npm/app/demo.ts
+++ HorseJs/npm/app/demo.ts
@@ -1,6 +1,8 @@
+import { horse } from '../src/main'
+import './common'
 let $ = (selector) => document.querySelector(selector)
-export let demo = {
-  init: () => {
+class Demo {
+  private menuEventInit() {
     $('#menuContainer').addEventListener('click', (e) => {
       let id = e.target.getAttribute('id')
       if (!id || e.target.classList.contains('menuSelected')) return
@@ -9,6 +11,8 @@ export let demo = {
       $('.sectionSelected').classList.remove('sectionSelected')
       $(`#${id.replace('menu', 'section')}`).classList.add('sectionSelected')
     })
+  }
+  private linkBtnEventInit() {
     $('#sectionCodeLink').addEventListener('click', () => {
       let codeFileName = $('.menuSelected').getAttribute('id').replace('menu', '').toLowerCase()
       let target = `https://gitee.com/horsejs/horsejs/blob/master/HorseJs/app/demo/${codeFileName}.js`
@@ -22,13 +26,26 @@ export let demo = {
       let target = `https://gitee.com/horsejs/horsejs/blob/master/Doc/Horse/${codeFileName}.md`
       horse.system.openExternal({ target, workingDir: '' })
     })
+  }
+  private demoBtnEventInit() {
     $('#demoContainer').addEventListener('click', async (e) => {
       if (!e.target.classList.contains('demoBtn')) return
       let id = e.target.getAttribute('id')
       let parentId = e.target.parentElement.getAttribute('id')
       parentId = parentId.replace('section', '').toLowerCase()
-      let { processor } = await import(`./demo/${parentId}.js`)
-      processor[id]()
+      //   let { processor } = await import(`./demo/${parentId}.js`)
+      //   processor[id]()
     })
-  },
+  }
+  async init() {
+    this.menuEventInit()
+    this.linkBtnEventInit()
+    this.demoBtnEventInit()
+    await horse.window.openDevTool()
+  }
+  constructor() {}
 }
+window.addEventListener('load', async () => {
+  let demo = new Demo()
+  demo.init()
+})
