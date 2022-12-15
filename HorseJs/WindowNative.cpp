84fb317ca8d627df1ea0a6e24df3432c452bb2f7
liuxiaolun <412588801@qq.com>
2022-12-15 16:45:17 +0800

    webview

---
+++ HorseJs/WindowNative.cpp
@@ -0,0 +1,95 @@
+#include "WindowNative.h"
+
+
+namespace {
+	// Pointer to WebViewController
+	static wil::com_ptr<ICoreWebView2Controller> webviewController;
+	// Pointer to WebView window
+	static wil::com_ptr<ICoreWebView2> webview;
+    static void Log(std::string& str) {
+        OutputDebugStringA(str.c_str());
+        OutputDebugStringA("\r\n");
+    }
+}
+using namespace Microsoft::WRL;
+WindowNative::WindowNative() {
+	auto envCB = [this](HRESULT result, ICoreWebView2Environment* env) -> HRESULT {
+		auto ctrlCBInstance = Callback<ICoreWebView2CreateCoreWebView2ControllerCompletedHandler>(this,&WindowNative::CreateBrowserView);
+		env->CreateCoreWebView2Controller(hWnd, ctrlCBInstance.Get());
+		return S_OK;
+	};
+	auto envCBInstance = Callback<ICoreWebView2CreateCoreWebView2EnvironmentCompletedHandler>(envCB);
+	CreateCoreWebView2EnvironmentWithOptions(nullptr, nullptr, nullptr, envCBInstance.Get());
+}
+
+HRESULT WindowNative::CreateBrowserView(HRESULT result, ICoreWebView2Controller* controller) {
+	if (controller != nullptr) {
+		webviewController = controller;
+		webviewController->get_CoreWebView2(&webview);
+	}
+	ConfigWebView();
+	// <NavigationEvents>
+	// Step 4 - Navigation events
+	// register an ICoreWebView2NavigationStartingEventHandler to cancel any non-https navigation
+	EventRegistrationToken token;
+	auto navigateCB = Callback<ICoreWebView2NavigationStartingEventHandler>(this, &WindowNative::NavigationStarting);
+	webview->add_NavigationStarting(navigateCB.Get(), &token);
+	// </NavigationEvents>
+	// <Scripting>
+	// Step 5 - Scripting
+	// Schedule an async task to add initialization script that freezes the Object object
+	webview->AddScriptToExecuteOnDocumentCreated(L"Object.freeze(Object);", nullptr);
+	// Schedule an async task to get the document URL
+	webview->ExecuteScript(L"window.document.URL;", Callback<ICoreWebView2ExecuteScriptCompletedHandler>(
+		[](HRESULT errorCode, LPCWSTR resultObjectAsJson) -> HRESULT {
+			LPCWSTR URL = resultObjectAsJson;
+	//doSomethingWithURL(URL);
+	return S_OK;
+		}).Get());
+	// </Scripting>
+
+	// Step 6 - Communication between host and web content
+	// Set an event handler for the host to return received message back to the web content
+	webview->add_WebMessageReceived(Callback<ICoreWebView2WebMessageReceivedEventHandler>(
+		[](ICoreWebView2* webview, ICoreWebView2WebMessageReceivedEventArgs* args) -> HRESULT {
+			wil::unique_cotaskmem_string message;
+	args->TryGetWebMessageAsString(&message);
+	// processMessage(&message);
+	webview->PostWebMessageAsString(message.get());
+	return S_OK;
+		}).Get(), &token);
+	// Schedule an async task to add initialization script that
+	// 1) Add an listener to print message from the host
+	// 2) Post document URL to the host
+	webview->AddScriptToExecuteOnDocumentCreated(
+		L"window.chrome.webview.addEventListener(\'message\', event => alert(event.data));" \
+		L"window.chrome.webview.postMessage(window.document.URL);",
+		nullptr);
+	webview->OpenDevToolsWindow();
+	return S_OK;
+}
+
+HRESULT WindowNative::NavigationStarting(ICoreWebView2* webview, ICoreWebView2NavigationStartingEventArgs* args){
+	wil::unique_cotaskmem_string uri;
+	args->get_Uri(&uri);
+	std::wstring source(uri.get());
+	if (source.substr(0, 5) != L"https") {
+		args->put_Cancel(true);
+	}
+	return S_OK;
+}
+void WindowNative::ConfigWebView() {
+	// Add a few settings for the webview
+	// The demo step is redundant since the values are the default settings
+	wil::com_ptr<ICoreWebView2Settings> settings;
+	webview->get_Settings(&settings);
+	settings->put_IsScriptEnabled(TRUE);
+	settings->put_AreDefaultScriptDialogsEnabled(TRUE);
+	settings->put_IsWebMessageEnabled(TRUE);
+	// Resize WebView to fit the bounds of the parent window
+	RECT bounds;
+	GetClientRect(hWnd, &bounds);
+	webviewController->put_Bounds(bounds);
+	// Schedule an async task to navigate to Bing
+	webview->Navigate(L"https://www.bing.com/");
+}
--- HorseJs/main.cpp
+++ HorseJs/main.cpp
@@ -1,37 +1,15 @@
 #include <windows.h>
-#include "Browser/BrowserProcessHandler.h"
-#include "Render/RenderProcessHandler.h"
-#include "Other/OtherProcessHandler.h"
+#include "WindowNative.h"
+
 
 //整个应用的入口函数
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
 {
-    CefEnableHighDPISupport();
-    //收集命令行参数
-    CefMainArgs mainArgs(hInstance);
-    CefRefPtr<CefCommandLine> commandLine = CefCommandLine::CreateCommandLine();
-    commandLine->InitFromString(::GetCommandLineW());//todo mac
-    //为不同进程创建不同的处理对象
-    CefRefPtr<CefApp> app;    
-    if (!commandLine->HasSwitch("type")) {
-        app = new BrowserProcessHandler();
+    WindowNative win;
+    MSG msg;
+    while (::GetMessageW(&msg, nullptr, 0, 0) == TRUE) {
+        ::TranslateMessage(&msg);
+        ::DispatchMessageW(&msg);
     }
-    else if (commandLine->GetSwitchValue("type").ToString() == "renderer") {
-        //--renderer-startup-dialog
-        app = new RenderProcessHandler();
-    }
-    else {
-        app = new OtherProcessHandler();
-    }
-    //启动子进程
-    int exit_code = CefExecuteProcess(mainArgs, app, nullptr);
-    if (exit_code >= 0) {
-        return exit_code;
-    }
-    //CEF消息循环
-    CefSettings settings;
-    CefInitialize(mainArgs, settings, app.get(), nullptr);
-    CefRunMessageLoop();
-    CefShutdown();
     return 0;
 }
