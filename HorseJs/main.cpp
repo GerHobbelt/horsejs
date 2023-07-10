9d763c8f9f75ba04849d15a88ed35d997e1d7ac2
liulun <xland@live.cn>
2023-07-10 15:07:49 +0800

    update

--- HorseJs/main.cpp
+++ HorseJs/main.cpp
@@ -1,11 +1,55 @@
 #include <windows.h>
 #include "WindowNative.h"
+#include <shellapi.h>
 
+
+
+bool checkOnRegKey(const HKEY& key, const std::wstring& subKey) {
+    bool hasRuntime = true;
+    size_t bufferSize = 18;
+    std::wstring valueBuf;
+    valueBuf.resize(bufferSize);
+    auto valueSize = static_cast<DWORD>(bufferSize * sizeof(wchar_t));
+    auto rc = RegGetValue(key, subKey.c_str(), L"pv", RRF_RT_REG_SZ, nullptr,
+        static_cast<void*>(valueBuf.data()), &valueSize
+    );
+    if (rc == ERROR_SUCCESS)
+    {
+        valueSize /= sizeof(wchar_t);
+        valueBuf.resize(static_cast<size_t>(valueSize - 1));
+        if (valueBuf.empty() || valueBuf == L"0.0.0.0") {
+            hasRuntime = false;
+        }
+    }
+    else
+    {
+        hasRuntime = false;
+    }
+    return hasRuntime;
+}
+
+bool checkRuntime()
+{
+    std::wstring regSubKey = L"\\Microsoft\\EdgeUpdate\\Clients\\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}";
+    bool hasRuntime = checkOnRegKey(HKEY_LOCAL_MACHINE, L"SOFTWARE\\WOW6432Node"+regSubKey);
+    if (hasRuntime) return true;
+    hasRuntime = checkOnRegKey(HKEY_CURRENT_USER, L"Software"+regSubKey);
+    if (!hasRuntime) {
+        auto result = MessageBox(nullptr, L"您得系统中缺少必要组件，现在为您打开下载链接，下载相关组件？", 
+            L"系统提示", MB_OKCANCEL | MB_ICONINFORMATION | MB_DEFBUTTON1);
+        if (result == IDOK) {
+            ShellExecute(0, 0, L"https://go.microsoft.com/fwlink/p/?LinkId=2124703", 0, 0, SW_SHOW);
+        }
+        return false;
+    }
+    return true;
+}
 
 //entry point
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPTSTR lpCmdLine, _In_ int nCmdShow)
 {
-    //https://learn.microsoft.com/zh-cn/microsoft-edge/webview2/concepts/distribution#detect-if-a-suitable-webview2-runtime-is-already-installed
+    auto flag = checkRuntime();
+    if (!flag) return 0;
     WindowNative win;
     MSG msg;
     while (GetMessage(&msg, nullptr, 0, 0) == TRUE) {
